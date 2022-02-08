79b46cc3212ee5937545e4679af6d4f20b011fc5
Loca55 <m.loca45@gmail.com>
2022-02-08 10:04:47 +0100

    update of Wang features

---
+++ image_similiraty_classic/fuzzyClustering.m
@@ -0,0 +1,63 @@
+function U = fuzzyClustering(image, maxIter)
+%FEATURES_WANG
+%compute segmentation with kmeans and L means for each segment
+image = imgaussfilt(image, 1.5);
+x = rgb2lch(image, 'lab');
+x = x(:,:,1);
+
+[rows, cols] = size(x);
+x = reshape(x, [rows*cols, 1]);
+
+nRegion = length(x);
+
+% centroid initialization 
+c0 = min(x);
+
+c = zeros(6,1);
+c(6) = max(x);
+for j = 1 : 5
+    c(j) = c0 + j * (c(6) + c0) / 6;
+end
+
+
+U = zeros(nRegion, 5);
+iter = 0;
+%set tollerance
+tol = 1e-5;
+relerr = inf;
+%compute U and update centroid
+while(relerr >= tol && iter < maxIter)
+    U = zeros(nRegion, 5);
+    cOld = c;
+    for i =1: nRegion
+        if x(i,1) <= c(1)
+            U(i,1) = 1;
+            U(i,2:5) = 0;
+        elseif x(i,1) > c(5)
+            U(i,5) = 1;
+            U(i,1:4) = 0;
+        end
+        for j = 1:5
+            if x(i,1) > c(j) && x(i) <= c(j+1)
+                U(i,j) = (c(j+1)-x(i,1)) / (c(j+1)-c(j))  ;
+                U(i,:) = 1-U(i,j);
+                U(i,j) = (c(j+1)-x(i,1)) / (c(j+1)-c(j))  ;
+            end
+        end
+    end
+
+    % update centroid
+    for j = 1 : 5
+        c(j) = (U(:,j)' * x) / sum(U(:,j));
+    end
+    
+    % compute error
+    relerr = norm(c - cOld) / norm(c);
+    
+    % increase counter
+    iter = iter + 1;
+end
+
+
+end
+
