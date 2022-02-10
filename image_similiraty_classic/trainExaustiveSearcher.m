57bbd6ac0d1190593cbab6f18d03da19532f981f
Federica Di Lauro <federicadilauro1998@gmail.com>
2022-02-10 21:28:53 +0100

    organize files and folders

--- image_similiraty_classic/trainExaustiveSearcher.m
+++
@@ -1,4 +0,0 @@
-load("../handcrafted_features_extractor/HandCraftedFeaturesPaintings.mat");
-
-%% create model for searching knn
-ex_searcher_trained = ExhaustiveSearcher(featuresVector(1:2000, :), 'distance', 'cosine');
