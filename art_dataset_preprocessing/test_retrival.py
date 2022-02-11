ed85bf4c34f25e37772b3a656815981951508d98
Federica Di Lauro <federicadilauro1998@gmail.com>
2022-02-11 09:31:32 +0100

    final csv for preprocessed dataset

--- art_dataset_preprocessing/test_retrival.py
+++ art_dataset_preprocessing/test_retrival.py
@@ -1,9 +1,9 @@
 import pandas as pd
 
 # Read the files into two dataframes.
-df = pd.read_csv('full_info.csv')
+df = pd.read_csv('img_db.csv')
 #df.set_index('filename', inplace = True)
-test = (df.loc[df['filename'] == "48177.jpg"])
+test = (df.loc[df['filename'] == "/home/fdila/repos/uni/plate2art/art_dataset_preprocessing/img/1_2.jpg"])
 print(test['artist'].values[0])
 print(test['title'].values[0])
 
